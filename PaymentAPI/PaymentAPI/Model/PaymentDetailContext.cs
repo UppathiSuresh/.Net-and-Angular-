4c7e4e6300bfadf37883c3f6303fbf52be2770dc
suresh.uppathi <suresh.uppathi@SPBLRLAP-4840>
2022-10-21 17:26:03 +0530

    API changes

---
+++ PaymentAPI/PaymentAPI/Model/PaymentDetailContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace PaymentAPI.Model
+{
+    public class PaymentDetailContext:DbContext
+    {
+        public PaymentDetailContext(DbContextOptions<PaymentDetailContext> option) : base(option)
+        { }
+            public DbSet<PaymentDetail> PaymentDetails { get; set; }
+
+        
+    }
+}
